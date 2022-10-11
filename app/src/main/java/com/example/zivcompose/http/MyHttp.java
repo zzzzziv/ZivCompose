bb8725c0545f49b878542c344932e84dacc5fe7e
ziv <1014863442@qq.com>
2022-10-11 23:31:23 +0800

    添加优雅的网络请求（okhttp3+retrofit2）

--- app/src/main/java/com/example/zivcompose/http/MyHttp.java
+++ app/src/main/java/com/example/zivcompose/http/MyHttp.java
@@ -21,7 +21,7 @@ public class MyHttp {
     private boolean toEntity = false;//是否转换成对象
 
     /**
-    * @Description: 这里使用了强转，有点不优雅
+    * @Description: get，这里使用了强转，有点不优雅
     * @Author: ziv
     * @Date: 2022/10/11
     */
@@ -36,9 +36,27 @@ public class MyHttp {
             future = new CompletableFuture<ResultJson>();
             http.Get(url,map).enqueue(new JsonCallback((CompletableFuture<ResultJson>) future));
         }
-
-
     }
+
+    /**
+     * @description:post
+     * @author: ziv
+     * @date: 2022/10/11 23:28
+     */
+    private <T>MyHttp(Post post,T bean){
+        this.url = post.url;
+        this.map = post.map;
+        this.toEntity = post.toEntity;
+        if (toEntity){
+            future = new CompletableFuture<ResultEntity<T>>();
+            http.Get(url,map).enqueue(new EntityCallback<>((CompletableFuture<ResultEntity<T>>)future,bean));
+        }else {
+            future = new CompletableFuture<ResultJson>();
+            http.Get(url,map).enqueue(new JsonCallback((CompletableFuture<ResultJson>) future));
+        }
+    }
+
+
 
     public String getUrl() {
         return url;
@@ -123,6 +141,72 @@ public class MyHttp {
             Map<String,String> map = new HashMap<>();
             for (int i=0;i<queryKey.length;i++){
                 map.put(queryKey[i],queryParams[i]);
+            }
+            this.map = map;
+            return new MyHttp(this,null);
+        }
+
+    }
+
+    public static class Post{
+        private String url;//请求url
+
+        private String[] postKey;//查询关键字
+
+        private String[] postParams;//查询值
+        private Map<String,String> map;//参数集合
+
+        private boolean toEntity;//是否转换对象
+        public Post(String url){
+            this.url = url;
+        }
+
+        /**
+         * @Description: 输入参数，可多个，要对应key值。
+         * @Param: 参数
+         * @Author: ziv
+         * @Date: 2022/10/11
+         */
+        public Post postParams(String ...params){
+            this.postParams = params;
+            return this;
+        }
+
+        /**
+         * @Description: 服务端规定的请求key值，与param对应。
+         * @Param:key
+         * @Author: ziv
+         * @Date: 2022/10/11
+         */
+        public Post postKey(String ...key){
+            this.postKey = key;
+            return this;
+        }
+
+        /**
+         * @Description: 转换成对应实体，需要给定实体对象
+         * @Author: ziv
+         * @Date: 2022/10/11
+         */
+        public <T>MyHttp toEntity(T bean){
+            this.toEntity = true;
+            Map<String,String> map = new HashMap<>();
+            for (int i=0;i<postKey.length;i++){
+                map.put(postKey[i],postParams[i]);
+            }
+            this.map = map;
+            return new MyHttp(this,bean);
+        }
+
+        /**
+         * @Description: 返回json对象
+         * @Author: ziv
+         * @Date: 2022/10/11
+         */
+        public MyHttp toJson(){
+            Map<String,String> map = new HashMap<>();
+            for (int i=0;i<postKey.length;i++){
+                map.put(postKey[i],postParams[i]);
             }
             this.map = map;
             return new MyHttp(this,null);
